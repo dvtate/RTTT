3c4081306079c4b6af84f330c08f10dba404920a
Dustin Van Tate Testa <toast27@gmail.com>
2017-04-27 21:31:50 -0400

    add theoretical code..

--- rttt.ino
+++ rttt.ino
@@ -2,6 +2,7 @@
 #include <FastLED.h>
 #include "grid.hpp"
 
+#define LED_DATA_PIN 10
 CRGB leds[81];
 
 Grid boards[3][3] {
@@ -20,12 +21,66 @@ Grid boards[3][3] {
   },
 };
 
+uint8_t buttonPins[3][3] {
+  { 1, 2, 3 }, { 4, 5, 6 },
+  { 7, 8, 9 }
+};
+
 void setup() {
 
   // assuming we have an unconnected analog pin...
   randomSeed(analogRead(0));
+
+  // mark button pins as input
+  for (uint8_t x = 0; x < 3; x++)
+    for (uint8_t y = 0; y < 3; y++)
+      pinMode(buttonPins[x][y]);
+
+  
+  // link the colors to the LEDs
+  FastLED.addLeds <WS2812B, LED_DATA_PIN, GRB>(leds, NUM_LEDS);
+
 }
 
 void loop() {
 
+  static possession_t curPlayer = RED;
+
+  // suggest a random square for each grid
+  for (uint8_t x = 0; x < 3; x++)
+    for (uint8_t y = 0; y < 3; y++)
+      boards[x][y].setRandPos(curPlayer);
+
+  // render and check for input
+  for (uint8_t i = 0; i < 3; i++) {
+    // set colors
+    for (uint8_t x = 0; x < 3; x++)
+      for (uint8_t y = 0; y < 3; y++)
+        boards[x][y].bindToLeds(i % 2);
+    FastLED.show();
+
+    // check for input
+    bool inputRecieved = false;
+    for (uint8_t g = 0; g < 100 && !inputRecieved; g++) {
+      
+      for (uint8_t x = 0; x < 3 && !inputRecieved; x++)
+        for (uint8_t y = 0; y < 3; y++)
+          if (digitalRead(buttonPins[x][y])) {
+            boards[x][y].keepSuggestion();
+            inputRecieved = true;
+          }
+          
+      delay(5);
+    }    
+    if (inputRecieved)
+      goto next_player;
+
+    
+  }
+
+next_player:
+  if (curPlayer == RED)
+    curPlayer = BLUE;
+  else
+    curPlayer = RED;
 }
